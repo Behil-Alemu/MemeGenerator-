dbe71ad9868e7278ca91159c90436aae595d555e
behilsamuel05@gmail.com <behilalemu@icloud.com>
2022-06-21 16:10:09 -0400

    organized js file and centered meme text on meme image

--- memeScript.js
+++ memeScript.js
@@ -2,7 +2,7 @@
 const inputLink = document.querySelector("input[name ='memeLink']")
 const inputTopText = document.querySelector("input[name ='topText']")
 const inputBottomText = document.querySelector("input[name ='bottomText']")
-const outPut = document.querySelector("#outPutImage")
+const outPut = document.querySelector(".outPutImage")
 const form = document.querySelector("#memeForm")
 
 
@@ -20,37 +20,32 @@ form.addEventListener("submit",function(e){
 
 
 function makeMeme(Link,TopText,BottomText){
-    const enteredMeme = document.createElement('img');
-    const top = document.createElement('h2');
-    const imageDiv = document.createElement('div');
-    // const newDiv = document.createElement('div');
-    const h2Div = document.createElement('div');
-    const bottom = document.createElement('h2');
-    const removeButton = document.createElement("button");
-    
-    h2Div.appendChild(top);
-    h2Div.appendChild(bottom);
-    outPut.appendChild(imageDiv);    
-    outPut.appendChild(h2Div);    
-    imageDiv.appendChild(enteredMeme);    
-    
-    imageDiv.appendChild(removeButton);
-    // imageDiv.appendChild(removeButton); 
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('memeDiv');
+    const imgMeme = document.createElement('img');
+    imgMeme.classList.add('image');
+    imgMeme.setAttribute('src', Link);
+    newDiv.append(imgMeme)
 
-    
-    removeButton.innerText = "X";
-    enteredMeme.classList.add('image');
+
+    const top = document.createElement('p');
     top.classList.add('topText');
-    bottom.classList.add('bottomText');
-    imageDiv.classList.add('imageDiv');
-    removeButton.classList.add('remove');
-    newDiv.classList.add("newDiv")
     top.innerText = TopText;
+    newDiv.append(top);
+
+    const bottom = document.createElement('p');
+    bottom.classList.add('bottomText');
     bottom.innerText= BottomText; 
+    newDiv.append(bottom);
+    
+    const removeButton = document.createElement("button");
+    removeButton.classList.add('remove');
+    removeButton.innerText = "X";
+    newDiv.append(removeButton);
 
-    enteredMeme.setAttribute('src', Link);
+    outPut.append(newDiv);
 
-    enteredMeme.innerHTML = Link; 
+
 
     removeButton.addEventListener("mouseover", function( event ){
         event.target.style.color = "gray";
@@ -58,9 +53,9 @@ function makeMeme(Link,TopText,BottomText){
             event.target.style.color = "";
         },1000);
     }, false); 
+}
 
-    removeButton.addEventListener('click', function(event){
-        event.preventDefault();
+    outPut.addEventListener('click', function(event){
         if (event.target.tagName === "BUTTON") {
         event.target.parentElement.remove();
       }
@@ -68,6 +63,4 @@ function makeMeme(Link,TopText,BottomText){
     }) 
     
     
-    return makeMeme 
-    
-}
+
